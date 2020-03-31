68c67912862780c9b82d88d45696666eed333b94
sagostinelli <batonenbois@gmail.com>
2020-03-31 13:27:31 -0400

    fleek rebrand

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import terminalLogo from './terminal-logo.png';
+import fleekLogo from './fleek-logo.png';
 import reactLogo from './react-logo.svg';
 
 import './App.css';
@@ -8,19 +8,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={terminalLogo} className="Terminal-logo" alt="terminal-logo" />
+        <img src={fleekLogo} className="Fleek-logo" alt="fleek-logo" />
         <span className="Big-plus">+</span>
         <img src={reactLogo} className="React-logo" alt="react-logo" />
         <p>
-          React App deployed on IPFS through Terminal
+          React App deployed on IPFS through Fleek
         </p>
         <a
           className="App-link"
-          href="https://docs.terminal.co"
+          href="https://docs.fleek.co"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn More About Terminal
+          Learn More About Fleek
         </a>
       </header>
     </div>
