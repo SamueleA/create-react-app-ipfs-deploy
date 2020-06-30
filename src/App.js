08fe90a581971f131904b6ce0f0f407806c03e0f
sagostinelli <batonenbois@gmail.com>
2020-06-30 13:23:31 -0400

    now with pr deploys

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <span className="Big-plus">+</span>
         <img src={reactLogo} className="React-logo" alt="react-logo" />
         <p>
-          React App deployed on IPFS through Fleek :)
+          React App deployed on IPFS through Fleek :) Now with pr deploys
         </p>
         <a
           className="App-link"
