50a865725fa0fc56a6e6b6c63367104fec638e98
sagostinelli <batonenbois@gmail.com>
2021-08-24 17:37:34 -0400

    push event

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn More About Fleek!
+          Learn More About Fleek!!!
         </a>
       </header>
     </div>
