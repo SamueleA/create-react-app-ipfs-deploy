51bc7e35363af3fd121efe35e6d60f7b72d93ca3
Joaquin Ayuso de Paul <joaxap@gmail.com>
2021-11-14 11:30:37 -0800

    Cleaned site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import fleekLogo from './fleek-logo.png';
-import reactLogo from './react-logo.svg';
 
 import './App.css';
 
@@ -8,20 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={fleekLogo} className="Fleek-logo" alt="fleek-logo" />
-        <span className="Big-plus">+</span>
-        <img src={reactLogo} className="React-logo" alt="react-logo" />
         <p>
-          React App deployed on IPFS through Fleek.
+          joaxap.eth
         </p>
-        <a
-          className="App-link"
-          href="https://docs.fleek.co"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn More About Fleek!
-        </a>
       </header>
     </div>
   );
